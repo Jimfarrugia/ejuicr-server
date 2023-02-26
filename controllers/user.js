4dff29b577d99c8fe845bdaf8dc51dd78323b53c
jimfarrugia <the.jim.farrugia@gmail.com>
2023-02-27 04:17:04 +1100

    add method for resetting password

--- controllers/user.js
+++ controllers/user.js
@@ -58,6 +58,57 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc      Create and send a reset password token
+// @route     POST /api/user/reset-password
+// @access    Private
+const resetPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  // Check user email
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("Unable to find an account with that email.");
+  }
+  const { id } = user;
+  // Generate reset password token
+  const token = generateResetPasswordToken(user.id);
+  // TODO - Send the token via email
+
+  res.status(200).json({
+    id,
+    email,
+    token,
+  });
+});
+
+// @desc      Get user data
+// @route     GET /api/user/me
+// @access    Private
+const updatePassword = asyncHandler(async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  const decodedToken = validateToken(token);
+  const { id } = decodedToken;
+  const user = await User.findOne({ _id: id });
+  if (!user) {
+    res.status(404);
+    throw new Error("Unable to find user account.");
+  }
+  if (!password || password.length < 6 || password.length > 250) {
+    res.status(400);
+    throw new Error("Password is missing or invalid.");
+  }
+  const hashedPassword = await hashPassword(password);
+  const updatedUser = await User.findByIdAndUpdate(
+    id,
+    { password: hashedPassword },
+    { new: true }
+  );
+  res.status(200).json({
+    updatedUser,
+  });
+});
+
 // @desc      Get user data
 // @route     GET /api/user/me
 // @access    Private
@@ -75,8 +126,31 @@ const hashPassword = async password => {
   return await bcrypt.hash(password, salt);
 };
 
+// Generate a login token
 const generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-module.exports = { registerUser, loginUser, getUser };
+// Generate a reset password token
+const generateResetPasswordToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+// Validate a reset password token
+const validateToken = token => {
+  return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(400);
+      throw new Error("Invalid or expired token.");
+    }
+    return decoded;
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  resetPassword,
+  getUser,
+  updatePassword,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUser } = require("../controllers/user");
+const {
+  registerUser,
+  loginUser,
+  getUser,
+  resetPassword,
+  updatePassword,
+} = require("../controllers/user");
 const { protect } = require("../middleware/auth");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
+router.post("/reset-password", resetPassword);
+router.post("/reset-password/:token", updatePassword);
 router.get("/me", protect, getUser);
 
 module.exports = router;
